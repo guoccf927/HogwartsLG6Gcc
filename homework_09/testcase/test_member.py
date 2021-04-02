aa0fc1ced57772d9a3b49b5bb648804d0aabc0c9
guoccf927 <guoccf@163.com>
2021-04-02 14:25:32 +0800

    第九次直播课 企业微信接口实战

--- homework_09/testcase/test_member.py
+++ homework_09/testcase/test_member.py
@@ -2,21 +2,55 @@
 # @Time   : 2021/4/1 17:22
 # @Author : guoccf
 # @File   : test_member.py
+import datetime
+import random
+
 from homework_09.wework.member_management import MemberManagement
 
 
 class TestMember:
     def setup(self):
         self.member = MemberManagement()
-        self.userid = "zhangsan"
-        self.name = "张三"
-        self.mobile = "+86 13311110000"
+
+        # 获取随机数
+        now_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        number = random.randint(11111111, 99999999)
+
+        # 用户信息
+        self.userid = f"zhangsan{now_time}"
+        self.name = f"张三{now_time}"
+        self.mobile = f"+86 133{number}"
         self.department = [1]
 
     def test_token(self):
-        print(self.member.get_token())
+        self.member.get_token()
 
     def test_create_member(self):
         res = self.member.create_member(self.userid, self.name, self.mobile, self.department)
-        # assert res["errmsg"] == "created"
-        assert res.get("errmsg", "network error") == "created"
+        assert res["errmsg"] == "created"
+        # 校验方式可用如下方式，避免网络原因
+        # assert res.get("errmsg", "network error") == "created"
+
+    def test_get_member_info(self):
+        # 添加用户
+        self.member.create_member(self.userid, self.name, self.mobile, self.department)
+        # 获取用户信息
+        res = self.member.get_member_info(self.userid)
+        # 删除用户
+        self.member.delete_member(self.userid)
+        # 校验userid
+        assert res["userid"] == self.userid
+
+    def test_update_member(self):
+        # 添加用户
+        self.member.create_member(self.userid, self.name, self.mobile, self.department)
+        # 更新昵称"alias"
+        alias = f"{self.name}昵称"
+        res = self.member.update_member(self.userid, alias)
+        # 获取用户信息
+        res_get = self.member.get_member_info(self.userid)
+        # 删除用户
+        self.member.delete_member(self.userid)
+        # 校验返回值
+        assert res["errmsg"] == "updated"
+        assert res_get["alias"] == alias
--- homework_09/wework/member_management.py
+++ homework_09/wework/member_management.py
@@ -4,19 +4,20 @@
 # @File   : member_management.py
 
 import requests
-
+from homework_09.wework.member_conf import *
 
 class MemberManagement:
     def __init__(self):
         self.access_token = self.get_token()
 
     def get_token(self):
-        corpid = "1970324977421399"
-        corpsecret = "pzZfYlQMM3zyE81-C1V7Oh-I_8OGbpbS3-WhN0urDyI"
+        # corpid:企业ID, corpsecret:应用的凭证密钥
+        corpid = TestConf["CORP_ID"]
+        corpsecret = TestConf["CONTACT_SYNC_SECRET"]
         get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}"
         r = requests.get(get_token_url)
         access_token = r.json()["access_token"]
-        print(access_token)
+        print(f"get_token响应结果\n {access_token}")
         return access_token
 
     def create_member(self, userid, name, mobile, department):
@@ -28,8 +29,8 @@ class MemberManagement:
             "department": department
         }
         r = requests.post(url=create_member_url, json=data)
-        print(r.json())
-        assert r.json()
+        print(f"create_member响应结果\n {r.json()}")
+        return r.json()
 
     def get_member_info(self, userid):
         # get 请求URL过长时，可将参数单独拎出来
@@ -39,19 +40,21 @@ class MemberManagement:
             "userid": userid
         }
         r = requests.get(url=get_member_info_url, params=params)
-        print(r.json())
+        print(f"get_member_info响应结果\n {r.json()}")
         return r.json()
 
-    def update_member(self):
+    def update_member(self, userid, alias):
         update_member_url = f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={self.access_token}"
         data = {
-            "userid": "zhangsan",
-            "alias": "编辑"
+            "userid": userid,
+            "alias": alias
         }
         r = requests.post(url=update_member_url, json=data)
-        print(r.json())
+        print(f"update_member响应结果\n {r.json()}")
+        return r.json()
 
     def delete_member(self, userid):
         delete_member_url = f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={self.access_token}&userid={userid}"
         r = requests.get(delete_member_url)
+        print(f"delete_member响应结果\n {r.json()}")
         return r.json()
